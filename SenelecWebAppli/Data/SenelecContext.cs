57716445da7ac3009053ce2d5f7852395169c4c8
Nafissatou DIAGNE <nafissatou.diack@senelec.sn>
2020-02-18 15:09:53 +0000

    Ajoutez des fichiers projet.

---
+++ SenelecWebAppli/Data/SenelecContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SenelecWebAppli.Models;
+
+namespace SenelecWebAppli.Data
+{
+    public class SenelecContext : DbContext
+    {
+        public SenelecContext (DbContextOptions<SenelecContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+
+
+        }
+
+        public DbSet<SenelecWebAppli.Models.Product> Product { get; set; }
+    }
+}
---
+++ SenelecWebAppli/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SenelecWebAppli.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
